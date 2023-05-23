417c372a9c33d6687fcfffc324af8352887d9769
Sheldon <aneln700@gmail.com>
2023-05-23 18:49:57 +0300

    Actual

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
- class PostmanTest {
+class PostmanTest {
 
     @Test
     void shouldReturnBody() {
